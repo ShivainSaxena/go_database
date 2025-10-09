d27e46e9df78c1b82d8bd3ef664453875fc53f02
Shivain Saxena <shivainsaxena@gmail.com>
2025-10-08 22:11:22 -0400

    Setup initial methods to set up the header for the node and implemented some testing

--- tree.go
+++ tree.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"testing",
-	"github.com/stretchr/testify/assert"
+	"encoding/binary"
+	"fmt"
 )
 
 /*
@@ -37,5 +37,50 @@ const BTREE_MAX_VAL_SIZE = 3000
 func init() {
 	// Total amount of space to store one maximum-sized key/value pair
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
-	assert(node1max <= BTREE_PAGE_SIZE)
+	if node1max > BTREE_PAGE_SIZE {
+        panic(fmt.Sprintf(
+            "FATAL: B-Tree node size calculation failed. Max size (%d bytes) exceeds page size (%d bytes).",
+            node1max,
+            BTREE_PAGE_SIZE,
+        ))
+    }
+}
+
+// will just be a chunk of bytes to make it easier to move data from memory to disk
+type BNode []byte
+
+type BTREE struct {
+	// pointer (nonzero page number)
+	root uint64
+	// callbacks to manage on-disk pages
+	get func(uint64) []byte // reads a page from disk
+	new func([]byte) uint64 // allocates and writes a new page (copy-on-write)
+	del func(uint64) // deallocates a page
+}
+
+const (
+	BNODE_NODE = 1 // internal nodes w/o values
+	BNODE_LEAF = 2 // leaf nodes w/ values
+)
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func main() {
+	test_node := BNode(make([]byte, HEADER))
+
+	test_node.setHeader(BNODE_LEAF, 5)
+
+	fmt.Println(test_node.btype())
+	fmt.Println(test_node.nkeys())
 }
